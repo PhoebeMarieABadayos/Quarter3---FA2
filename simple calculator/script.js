75d6a60834c65b6d1085dbb320ffd6848239769f
PhoebeMarieABadayos <phoebemarie.badayos@smc.pshs.edu.ph>
2025-02-11 20:16:25 +0800

    Update script.js

--- simple calculator/script.js
+++ simple calculator/script.js
@@ -1,31 +1,31 @@
-function addition(+)
+function addition()
 {
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     document.getElementById("result").innerHTML = "The sum of " + num1 + " and " + num2 + " is " + (parseInt(num1) + parseInt(num2)) + ".";
 }
 
-function subtraction(-)
+function subtraction()
 {
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     document.getElementById("result").innerHTML = "The difference of " + num1 + " and " + num2 + " is " + (parseInt(num1) - parseInt(num2)) + ".";
 }
 
-function multiplication(*)
+function multiplication()
 {
     document.getElementById("result").innerHTML = "The product of " + num1 + " and " + num2 + " is " + (parseInt(num1)*parseInt(num2)) + ".";
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
 }
 
-function division(/)
+function division()
 {
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     document.getElementById("result").innerHTML = "The quotient of " + num1 + " and " + num2 + " is " + (parseInt(num1)/parseInt(num2)) + ".";
 }
-function modulus(%)
+function modulus()
 {
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
